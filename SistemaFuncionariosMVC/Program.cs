7b238b19dd6b5387339dd67986be8d20e3a2ac3a
kessiamilena <kessia.milena124@yahoo.com>
2025-11-07 07:59:10 -0300

    Correção de erros na conexão com banco de dados

--- SistemaFuncionariosMVC/Program.cs
+++ SistemaFuncionariosMVC/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaFuncionariosMVC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao"))); 
 
 var app = builder.Build();
 
@@ -22,7 +28,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Funcionario}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
